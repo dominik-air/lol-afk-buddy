5426298a91ec2250937b683c9ef08a078ca13e5a
dominik-air <62311964+dominik-air@users.noreply.github.com>
2021-09-29 17:53:43 +0200

    apiscraper version fix

--- utillity/apiscraper.py
+++ utillity/apiscraper.py
@@ -2,8 +2,11 @@ import requests
 import json
 
 
-summoners_data = requests.get("http://ddragon.leagueoflegends.com/cdn/11.19.1/data/en_US/summoner.json").json()["data"]
-champions_data = requests.get("http://ddragon.leagueoflegends.com/cdn/11.19.1/data/en_US/champion.json").json()["data"]
+# FIXME: LCU driver is capable of updating this data, maybe rework?
+version = "11.19.1"
+
+summoners_data = requests.get(f"http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/summoner.json").json()["data"]
+champions_data = requests.get(f"http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json").json()["data"]
 
 # get the summoner spells' names available on Summoner's Rift
 summoner_spells = [(spell, summoners_data[spell]['key']) for spell in summoners_data.keys() if "CLASSIC" in summoners_data[spell]["modes"]]
@@ -13,7 +16,7 @@ with open("../data/summoner_spells.json", "w+") as output:
     json.dump(summoner_spells, output, indent=4)
 # get the summoners_data spells' icons
 for (summoner_spell, _) in summoner_spells:
-    icon = requests.get(f"http://ddragon.leagueoflegends.com/cdn/11.16.1/img/spell/{summoner_spell}.png")
+    icon = requests.get(f"http://ddragon.leagueoflegends.com/cdn/{version}/img/spell/{summoner_spell}.png")
     if icon.status_code == 200:
         # FIXME: the file path needs a rework
         with open(f"../img/summoner_spells_icons/{summoner_spell}.png", "wb+") as output_icon:
@@ -29,7 +32,7 @@ with open("../data/champions.json", "w+") as output:
     json.dump(champions, output, indent=4)
 # get the champions' icons
 for champion in champions:
-    icon = requests.get(f"http://ddragon.leagueoflegends.com/cdn/11.16.1/img/champion/{champion}.png")
+    icon = requests.get(f"http://ddragon.leagueoflegends.com/cdn/{version}/img/champion/{champion}.png")
     if icon.status_code == 200:
         # FIXME: the file path needs a rework
         with open(f"../img/champion_images/{champion}.png", "wb+") as output_icon:
